b4955d87a13764838e161caf1fa5f8f5d39e6863
TroniX961 <tobiasmalessa2013@gmail.com>
2025-05-26 17:41:50 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands, tasks
 import os
 import datetime
+import requests
+from bs4 import BeautifulSoup
+
 
 intents = discord.Intents.default()
 intents.message_content = True
